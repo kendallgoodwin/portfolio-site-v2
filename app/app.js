cd93ba1fc6e6215d9cd8e14b56ae86d95e61cb1a
kendallgoodwin <kendall@kendallgoodwin.com>
2016-08-25 14:06:51 -0700

    routing for home and about views

--- app/app.js
+++ app/app.js
@@ -1,1 +1,19 @@
 var app = angular.module('PortfolioApp', ['PortfolioCtrl', 'ui.router'])
+
+.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+  .state('Home', {
+  	url: '/',
+  	templateUrl: 'home.html',
+  	controller: 'HomeCtrl'
+  })
+  $stateProvider
+  .state('About', {
+  	url: '/about',
+  	templateUrl: 'about.html',
+  	controller: 'AboutCtrl'
+  })
+
+}]);
--- app/controllers.js
+++ app/controllers.js
@@ -3,3 +3,7 @@ angular.module('PortfolioCtrl', ['PortfolioServices', 'ui.bootstrap'])
 app.controller('NavbarCtrl', ['$scope', function($scope) {
 
 }])
+
+app.controller('HomeCtrl', ['$scope', function($scope) {
+	$state.go('Home')
+}])
