223e7eee55fc5e9e5cd642fdece78b9353bb568e
kendallgoodwin <kendall@kendallgoodwin.com>
2016-09-09 12:26:59 -0700

    added collapse functionality to project images, having some issues

--- app/controllers.js
+++ app/controllers.js
@@ -15,5 +15,28 @@ app.controller('AboutCtrl', ['$scope', '$state', function($scope, $state) {
 }])
 
 app.controller('ProjectsCtrl', ['$scope', '$state', function($scope, $state) {
+	$scope.isCollapsed = true;
+	$scope.isCollapsedHorizontal = true;
+
+// 	  $scope.imageList = [
+//     {
+//       url: '/images/kgmarried.png',
+//       name: 'kgmarried'
+//     },
+//     {
+//       url: '/images/homeventory.png',
+//       name: 'homeventory'
+//     },
+//     {
+//       url: '/images/bestever.png',
+//       name: 'bestever'
+//     },
+//     {
+//       url: '/images/memory.png',
+//       name: 'memory'
+//     }
+//   ]
+// })
+
 	$state.go('Projects')
 }])
